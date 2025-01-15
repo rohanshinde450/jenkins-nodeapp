441918d5989347f36dde628b237cc1ca7368d08b
rohanshinde450 <rpshinde450@gmail.com>
2025-01-15 12:43:57 +0530

    naya

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('successfully build ');
+ res.send('Hello World');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
